76122d6efbd8f485bcc0a40e6d10a0493976863c
annvt <nguyenvantyan@gmail,com>
2023-09-22 22:22:21 +0700

    update

--- vuejs/vue-course/vueintro/main10.js
+++ vuejs/vue-course/vueintro/main10.js
@@ -1,3 +1,70 @@
+Vue.component('product-review', {
+	template: `
+		<form class="review-form" @submit.prevent="onSubmit">
+
+		<p v-if="errors.length">
+			<b>Please fixe following error(s):</b>
+			<ul>
+				<li v-for="error in errors">{{ error }}</li>
+			</ul>
+		</p>
+
+		<p>
+			<label for="name">Name:</label>
+			<input v-model="name">
+		</p>
+
+		<p>
+			<label for="review">Review:</label>
+			<textarea id="review" v-model="review"></textarea>
+		</p>
+
+		<p>
+			<label for="rating"></label>
+			<select id="rating" v-model="rating">
+				<option>5</option>
+				<option>4</option>
+				<option>3</option>
+				<option>2</option>
+				<option>1</option>
+			</select>
+		</p>
+
+		<input type="submit">
+
+		</form>
+	`,
+	data(){
+		return {
+			name: null,
+			review: null,
+			rating: null,
+			errors: []
+		}
+	},
+	methods: {
+		onSubmit(){
+			if(this.name && this.rating && this.review){
+				let productReview = {
+					name: this.name,
+					review: this.review,
+					rating: this.rating
+				}
+				this.$emit('review-submitted', productReview)
+				this.name = null
+				this.review = null
+				this.rating = null
+				this.errors = []
+			} else {
+				if(!this.name) this.errors.push('Name cannot be blank')
+				if(!this.review) this.errors.push('Review cannot be blank')
+				if(!this.rating) this.errors.push('Rating cannot be blank')
+			}
+			
+		}
+	}
+})
+
 Vue.component('product-detail',{
 	props: {
 		details: {
@@ -54,6 +121,20 @@ Vue.component('product', {
 
 			</div>
 
+			<div>
+				<h2>Reviews</h2>
+				<p v-if="!reviews.length">Chua co review. Hay la nguoi dau tien!</p>
+				<ul>
+					<li v-for="review in reviews">
+						<p>{{ review.name }}</p>
+						<p>{{ review.rating }}</p>
+						<p>{{ review.review }}</p>
+					</li>
+				</ul>
+			</div>
+
+			<product-review @review-submitted="adReview"></product-review>
+
 		</div>	
 	`,
 	data(){
@@ -86,7 +167,8 @@ Vue.component('product', {
 					variantImage: "./assets/product2.jpg",
 					variantQuantity: 0
 				}
-			]
+			],
+			reviews: []
 			
 		}
 	}, 
@@ -100,7 +182,11 @@ Vue.component('product', {
 		updateProduct(index){
 			this.selectedVariant = index
 			console.log(index)
+		},
+		adReview(productReview){
+			this.reviews.push(productReview)
 		}
+
 	},
 	computed: {
 		title(){
