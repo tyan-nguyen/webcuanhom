485bd14f4f310c55c20a52c13e7bb28d52220bb3
Nvtan <nguyenvantyan@gmail.com>
2026-03-16 21:27:00 +0700

    cap nhat git inorge

--- config/params.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'adminEmail' => 'admin@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
-    'bsVersion' => '5.x', // this will set globally `bsVersion` to Bootstrap 5.x for all Krajee Extensions
-    'webUrl' => 'http://quanlycua.nguyentrinh.com/',
-];
